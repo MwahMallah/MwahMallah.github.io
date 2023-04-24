8d51b3b0b4c9037ec9597733b5ff9b26096932b4
MwahMallah <82042468+MwahMallah@users.noreply.github.com>
2023-04-24 02:05:26 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', function() {
+    var header_wrapper = document.querySelector('.header__wrapper');
+    var header_name = document.querySelector('.header__name');
+    var header_links = document.querySelectorAll('.header__link');
+
+    if (window.scrollY > 0) {
+        header_wrapper.classList.add('scrolled');
+        header_name.classList.add('scrolled');
+
+        header_links.forEach(function(header_link) {
+            header_link.classList.add('scrolled');
+        });
+    } 
+    else {
+        header_wrapper.classList.remove('scrolled');
+        header_name.classList.remove('scrolled');
+        
+        header_links.forEach(function(header_link) {
+            header_link.classList.remove('scrolled');
+        });
+    }
+  });
+
+const menuButton = document.querySelector('.header__bars');
+const menu = document.querySelector('.mobile-nav');
+
+menuButton.addEventListener('click', function() {
+  menu.classList.toggle('show');
+});
